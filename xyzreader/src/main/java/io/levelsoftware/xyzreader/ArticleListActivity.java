03a4aeb03e1a5b8555a9ff960b4069f6ee2801ee
Victor Rendina <vrendina@gmail.com>
2017-05-04 15:46:22 -0400

    Added menu

--- xyzreader/src/main/java/io/levelsoftware/xyzreader/ArticleListActivity.java
+++ xyzreader/src/main/java/io/levelsoftware/xyzreader/ArticleListActivity.java
@@ -1,16 +1,22 @@
 package io.levelsoftware.xyzreader;
 
 import android.os.Bundle;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class ArticleListActivity extends AppCompatActivity {
+public class ArticleListActivity extends AppCompatActivity implements ArticleListAdapter.ArticleListClickHandler {
 
-    @BindView(R.id.rv_article_list)
-    RecyclerView articleRecyclerView;
+    @BindView(R.id.cl_list) CoordinatorLayout listCoordinatorLayout;
+    @BindView(R.id.tb_list_header) Toolbar listToolbar;
+    @BindView(R.id.rv_article_list) RecyclerView articleRecyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +25,40 @@ public class ArticleListActivity extends AppCompatActivity {
 
         ButterKnife.bind(this);
 
-        articleRecyclerView.setAdapter(new ArticleListAdapter());
+        setSupportActionBar(listToolbar);
+        if(getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayShowTitleEnabled(false);
+        }
+
+        articleRecyclerView.setAdapter(new ArticleListAdapter(this));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_list, menu);
+        return true;
+    }
+
+    @Override
+    public void clickListItem(int position) {
+
+    }
+
+    @Override
+    public void clickBookmark(int position) {
+        Snackbar.make(listCoordinatorLayout, R.string.action_added_bookmark, Snackbar.LENGTH_SHORT).show();
+
+    }
+
+    @Override
+    public void clickFavorite(int position) {
+        Snackbar.make(listCoordinatorLayout, R.string.action_added_favorite, Snackbar.LENGTH_SHORT).show();
+
+    }
+
+    @Override
+    public void clickShare(int position) {
+
     }
 }
--- xyzreader/src/main/java/io/levelsoftware/xyzreader/ArticleListAdapter.java
+++ xyzreader/src/main/java/io/levelsoftware/xyzreader/ArticleListAdapter.java
@@ -9,18 +9,24 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
-import timber.log.Timber;
 
 public class ArticleListAdapter extends RecyclerView.Adapter<ArticleListAdapter.ArticleCardViewHolder> {
+
+    private ArticleListClickHandler clickHandler;
+
+    public ArticleListAdapter(ArticleListClickHandler clickHandler) {
+        this.clickHandler = clickHandler;
+    }
 
     @Override
     public ArticleCardViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         LayoutInflater inflater = LayoutInflater.from(parent.getContext());
-        View view = inflater.inflate(R.layout.article_list_item, parent, false);
+        View view = inflater.inflate(R.layout.list_item_article, parent, false);
 
         return new ArticleCardViewHolder(view);
     }
@@ -29,6 +35,8 @@ public class ArticleListAdapter extends RecyclerView.Adapter<ArticleListAdapter.
     public void onBindViewHolder(ArticleCardViewHolder holder, int position) {
 
         holder.titleTextView.setText("Testing!!!");
+        holder.authorTextView.setText("Victor Rendina");
+        holder.dateTextView.setText("Jun 16, 2016");
 
     }
 
@@ -39,14 +47,14 @@ public class ArticleListAdapter extends RecyclerView.Adapter<ArticleListAdapter.
 
     class ArticleCardViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
 
-        @BindView(R.id.cv_list_item)
-        CardView itemCardView;
-
-        @BindView(R.id.fl_list_item)
-        FrameLayout itemFrameLayout;
-
-        @BindView(R.id.tv_list_title)
-        TextView titleTextView;
+        @BindView(R.id.cv_list_item) CardView itemCardView;
+        @BindView(R.id.fl_list_item) FrameLayout itemFrameLayout;
+        @BindView(R.id.tv_list_title) TextView titleTextView;
+        @BindView(R.id.tv_list_author) TextView authorTextView;
+        @BindView(R.id.tv_list_date) TextView dateTextView;
+        @BindView(R.id.iv_list_add_bookmark) ImageView bookmarkImageView;
+        @BindView(R.id.iv_list_add_favorite) ImageView favoriteImageView;
+        @BindView(R.id.iv_list_share) ImageView shareImageView;
 
         ArticleCardViewHolder(View view) {
             super(view);
@@ -54,21 +62,47 @@ public class ArticleListAdapter extends RecyclerView.Adapter<ArticleListAdapter.
             ButterKnife.bind(this, view);
 
             itemFrameLayout.setOnClickListener(this);
+            bookmarkImageView.setOnClickListener(this);
+            favoriteImageView.setOnClickListener(this);
+            shareImageView.setOnClickListener(this);
         }
 
         @Override
         public void onClick(View v) {
-            Timber.d("Clicked on view!!!" + getAdapterPosition());
+            switch(v.getId()) {
+                case R.id.fl_list_item:
+                    // Lift the card if we are on Lollipop or greater
+                    if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP ) {
+                        AnimatorSet liftAnimator =
+                                (AnimatorSet) AnimatorInflater.loadAnimator(v.getContext(), R.animator.lift_on_touch);
 
-            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP ) {
-                AnimatorSet liftAnimator =
-                        (AnimatorSet)AnimatorInflater.loadAnimator(v.getContext(), R.animator.lift_on_touch);
+                        liftAnimator.setTarget(itemCardView);
+                        liftAnimator.start();
+                    }
 
-                liftAnimator.setTarget(itemCardView);
-                liftAnimator.start();
+                    clickHandler.clickListItem(getAdapterPosition());
+                break;
+
+                case R.id.iv_list_add_bookmark:
+                    clickHandler.clickBookmark(getAdapterPosition());
+                break;
+
+                case R.id.iv_list_add_favorite:
+                    clickHandler.clickFavorite(getAdapterPosition());
+                break;
+
+                case R.id.iv_list_share:
+                    clickHandler.clickShare(getAdapterPosition());
+                break;
             }
         }
     }
 
+    public interface ArticleListClickHandler {
+        void clickListItem(int position);
+        void clickBookmark(int position);
+        void clickFavorite(int position);
+        void clickShare(int position);
+    }
 
 }
